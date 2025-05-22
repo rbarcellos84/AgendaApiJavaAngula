261b963b3a19739628c6c631948cf460f3cfaedf
rbarcellos84 <rbarcellos84@gmail.com>
2025-05-21 22:12:05 -0300

    Criando projeto web

---
+++ Web/projetoAngendaWeb/src/app/components/pages/cadastro-tarefas/cadastro-tarefas.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-tarefas',
+  imports: [],
+  templateUrl: './cadastro-tarefas.component.html',
+  styleUrl: './cadastro-tarefas.component.css'
+})
+export class CadastroTarefasComponent {
+
+/*
+Escreva um componente em angular para cadastrar tarefas em uma agenda (usando bootstrap 5). Não precisa implementar o funcionamento do formulário, apenas desenhe o HTML.
+O formulário deverá ter os campos:
+Titulo da tarefa (campo de texto)
+Data e hora (campo de data e hora)
+Finalizado (campo sim ou não)
+Categoria (campo para que o usuário selecione a categoria desejada)
+Desenhe um card na página com largura 100% de largura e com sombra
+Nao adicione css use apenas bootstrap
+*/
+
+}
---
+++ Web/projetoAngendaWeb/src/app/components/shared/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+/*
+Desenhe um componente angular contendo um navbar (bootstrap 5) para um projeto chamado Agenda de tarefas. Inclua neste navbar links para cadastro e consulta de tarefas. coloque-o na cor azul e não use css adicional apenas o bootstrap
+*/
+
+}
