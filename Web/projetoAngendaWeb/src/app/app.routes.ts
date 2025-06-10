b190989320f525a0f8b747b6ac74bb8ca4800115
rbarcellos84 <rbarcellos84@gmail.com>
2025-06-10 08:55:47 -0300

    Update dashboard

--- Web/projetoAngendaWeb/src/app/app.routes.ts
+++ Web/projetoAngendaWeb/src/app/app.routes.ts
@@ -2,8 +2,14 @@ import { Routes } from '@angular/router';
 import { CadastroTarefasComponent } from './components/pages/cadastro-tarefas/cadastro-tarefas.component';
 import { ConsultaTarefasComponent } from './components/pages/consulta-tarefas/consulta-tarefas.component';
 import { EdicaoTarefasComponent } from './components/pages/edicao-tarefas/edicao-tarefas.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
+    {
+        path: 'pages/dashboard', //rota de navegacao
+        component: DashboardComponent, //componente
+        pathMatch: 'full' // Garante que o path seja exatamente vazio
+    },
     {
         path: 'pages/cadastro-tarefas', //rota de navegacao
         component: CadastroTarefasComponent //componente
@@ -15,5 +21,10 @@ export const routes: Routes = [
     {
         path: 'pages/edicao-tarefas/:id', //rota de navegação
         component: EdicaoTarefasComponent //componente
+    },
+    {
+        // Opcional: Rota curinga para lidar com caminhos não encontrados (404)
+        path: '**',
+        redirectTo: 'pages/dashboard'
     }
 ];
